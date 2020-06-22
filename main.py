c61b2fc6c1f55af9627a0dff78109bdf7a332b21
asefehgj <asefehgj@stud.ntnu.no>
2020-06-22 15:07:06 +0200

    sitste

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to groupping program!")
+name = input("What is your name? ")
+age = int(input("what is your age? "))
+health = 10
+if age >= 18:
+    print (name, "You are old enough to play!")
